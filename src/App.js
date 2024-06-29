e9a17818627a97fca8ce3db7896efc90b5113976
kmiqbal19 <km.iqbal01@gmail.com>
2024-06-29 15:55:19 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./App.css";
 import { Box, TextField, Button, Typography } from "@mui/material";
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
-import axios from 'axios'
-
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import CircularProgress from "@mui/material/CircularProgress";
+import { toast } from "sonner";
 function App() {
   const [keyword, setKeyword] = React.useState("");
-  const handleClick = async (_) => {
-    // call here the api from backend
-   const res = await axios.post('http://127.0.0.1:5000/', {user_input: keyword});
-   console.log(res.data, 'response')
-  }
+  const [loading, setLoading] = React.useState(false);
+
+  const handleClick = (_) => {
+    setLoading(true);
+    setKeyword("")
+    axios
+      .post("http://127.0.0.1:5000/", { user_input: keyword })
+      .then((res) => {
+        console.log(res.data, "res");
+        setLoading(false);
+      })
+      .catch((err) => toast.error("Something went wrong!"))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  console.log(loading, "loading");
   return (
     <Box
       sx={{
@@ -23,9 +37,15 @@ function App() {
         flexDirection: "column",
       }}
     >
-      <Typography variant="h6" sx={{
-        marginBottom: '1rem',
-      }}>Search for PubMed documents</Typography>
+      <Typography
+        variant="h6"
+        sx={{
+          marginBottom: "1rem",
+        }}
+      >
+        Search for PubMed documents
+      </Typography>
+      {loading && <CircularProgress sx={{margin: '0.5rem'}} />}
       <TextField
         sx={{
           width: "calc(300px + 5vw)",
@@ -34,10 +54,11 @@ function App() {
         id="outlined-required"
         label="Search"
         defaultValue={keyword}
+        value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         InputProps={{
           endAdornment: (
-            <InputAdornment position="start" sx={{marginLeft: '0.5rem'}} >
+            <InputAdornment position="start" sx={{ marginLeft: "0.5rem" }}>
               <SearchIcon />
             </InputAdornment>
           ),
@@ -50,6 +71,7 @@ function App() {
         }}
         variant="contained"
         onClick={handleClick}
+        disabled={loading}
       >
         Submit
       </Button>
