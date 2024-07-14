777c7e4c6d711b808b74cf51b8a517531d67de6a
kmiqbal19 <km.iqbal01@gmail.com>
2024-07-14 20:00:41 +0200

    added topics

--- src/utils.js
+++ src/utils.js
@@ -3,16 +3,16 @@ export const transformData = (data) => {
     const clusters = {};
   
     data.forEach(item => {
-      const { cluster, PMID } = item;
+      const { cluster, PMID, TOPIC } = item;
       if (!clusters[cluster]) {
         clusters[cluster] = [];
       }
-      clusters[cluster].push(PMID);
+      clusters[cluster].push({PMID, TOPIC});
     });
-  
+     
     const formattedData = Object.keys(clusters).map(cluster => ({
       label: `Cluster ${cluster}`,
-      groups: clusters[cluster].map(PMID => ({ label: PMID }))
+      groups: clusters[cluster].map(({PMID, TOPIC}) => ({ label: TOPIC }))
     }));
   
     return formattedData;
